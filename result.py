6be6236582eb7d576ef9ae81c9834d407eef0b4a
Edvards Daniels Volfs <edvards.volfs2015@gmail.com>
2023-09-07 20:13:53 +0300

    simplify

--- result.py
+++ result.py
@@ -4,21 +4,11 @@ def main():
     tip = dollars * percent
     print(f"Leave ${tip:.2f}")
 
-def dollars_to_float(d):
-   
-    try: 
+def dollars_to_float(d): 
         dollars_float = float(d)
         return dollars_float
-    except ValueError:
-        print("Invalid input. Please enter a valid number.")
-        return 0.0  
-
 def percent_to_float(p):
-    
-    try:
         percent_float = float(p) / 100.0  
         return percent_float
-    except ValueError:
-        print("Invalid input. Please enter a valid number.")
-        return 0.0  
+    
 main ()
